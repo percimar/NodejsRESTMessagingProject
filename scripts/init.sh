64b47eb09c8cafd3c28b4494f767642ae51231f4
Asmar Siddiqui <60093530@cna-qatar.edu.qa>
2021-04-01 20:18:44 +0300

    moved to /tmp/ instead of /opt

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,8 +2,8 @@
 echo Starting init script
 pwd
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
-cp -r . /opt/TestChatApp
-cd /opt/TestChatApp
+cp -r . /tmp/TestChatApp
+cd /tmp/TestChatApp
 npm install
 systemctl start testchatapp
 # node server.js 3001 chatapp 12class34 testjenkins
