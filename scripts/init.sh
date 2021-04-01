62b36c0f2c36cc39414ca6087996d93e2a74c9db
Asmar Siddiqui <60093530@cna-qatar.edu.qa>
2021-04-01 20:14:46 +0300

    fixed maybe?

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,6 +5,7 @@ mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
 cp -r . /opt/TestChatApp
 cd /opt/TestChatApp
 npm install
-node server.js 3001 chatapp 12class34 testjenkins
+systemctl start testchatapp
+# node server.js 3001 chatapp 12class34 testjenkins
 npm test
 echo End of init script
