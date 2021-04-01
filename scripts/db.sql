a2c564e17daf294e07cf54e08a2391bb844f7fc1
Asmar Siddiqui <60093530@cna-qatar.edu.qa>
2021-04-01 19:17:38 +0300

    db script

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 31, 2021 at 03:02 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.1
+DROP DATABASE IF EXISTS `jenkins`;
+CREATE DATABASE `jenkins`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `jenkins`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
+DROP TABLE IF EXISTS `messages`;
 CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `message` varchar(255) NOT NULL,
@@ -35,9 +10,6 @@ CREATE TABLE `messages` (
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `messages`
---
 
 INSERT INTO `messages` (`id`, `message`, `sender`, `receiver`, `date`) VALUES
 (19, 'whatup admin', 3, 1, '2021-03-31 11:13:46'),
@@ -46,32 +18,7 @@ INSERT INTO `messages` (`id`, `message`, `sender`, `receiver`, `date`) VALUES
 (74, 'helo aahmad', 4, 1, '2021-03-31 12:58:41'),
 (75, 'helo aahmad`\n`', 4, 3, '2021-03-31 12:58:59');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `msg`
---
-
-CREATE TABLE `msg` (
-  `id` int(11) NOT NULL,
-  `sender` varchar(255) NOT NULL,
-  `receiver` varchar(255) NOT NULL,
-  `message` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `msg`
---
-
-INSERT INTO `msg` (`id`, `sender`, `receiver`, `message`) VALUES
-(1, 'admin', 'admin', 'dfg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
@@ -79,9 +26,6 @@ CREATE TABLE `users` (
   `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (1, 'admin', '17af9a2a5ce9c3b6c35619eedb747fa254382578', 'admin'),
@@ -90,64 +34,25 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
 (4, 'asmar', '17af9a2a5ce9c3b6c35619eedb747fa254382578', 'regular'),
 (5, 'joe', '17af9a2a5ce9c3b6c35619eedb747fa254382578', 'regular');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `messages`
---
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`),
   ADD KEY `receiver` (`receiver`),
   ADD KEY `sender` (`sender`);
 
---
--- Indexes for table `msg`
---
-ALTER TABLE `msg`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `messages`
---
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
---
--- AUTO_INCREMENT for table `msg`
---
-ALTER TABLE `msg`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `messages`
---
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`receiver`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`sender`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
