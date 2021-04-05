e8c3dc70147419eff944aed613a5e39fc5fcca0e
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 15:13:28 +0300

    ctrl z

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 echo Starting init script
 set -e
-echo $USER;
-whoami;
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
 cp server.js testing.test.js puppeteer.js package.json package-lock.json .eslintrc.js webfiles/ /tmp/TestChatApp -r
 echo End of init script
