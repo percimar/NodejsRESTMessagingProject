17d24d07b7e770e7655cc726b843b728bebdddb6
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 13:41:16 +0300

    added some logging

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,8 +2,7 @@
 echo Starting init script
 set -e
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
-mysql -u chatapp -p12class34 jenkins < ./scripts/db.sql
-cp -r . /tmp/TestChatApp
+cp -r .  /tmp/TestChatApp
 cd /tmp/TestChatApp
 npm install
 npm test
