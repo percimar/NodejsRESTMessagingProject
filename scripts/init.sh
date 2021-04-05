4facb39d523c8fb231311b47f388b4b1a4430683
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 15:11:42 +0300

    yeah this is annoying

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo Starting init script
 set -e
+echo $USER;
+whoami;
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
 cp server.js testing.test.js puppeteer.js package.json package-lock.json .eslintrc.js webfiles/ /tmp/TestChatApp -r
 echo End of init script
