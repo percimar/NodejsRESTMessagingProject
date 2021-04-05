5db2faaa1a7bd19afa751c5f7da77995d665ab05
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 14:10:24 +0300

    fix mv

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 echo Start of deploy script
 set -e
 mv server.js testing.test.js package.json package-lock.json /opt/ChatApp 
-mv webfiles/* /opt/ChatApp/ -r
+mv webfiles/* /opt/ChatApp/ 
 echo End of deploy script
