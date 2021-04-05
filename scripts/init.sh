647b56df1f9ca19e6348f575b45e09c153be5ed7
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 13:14:39 +0300

    Removed one-time script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 echo Starting init script
 set -e
-mv server.js testing.test.js package.json package-lock.json webfiles/ /opt/ChatApp
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
 cp -r ./ /tmp/TestChatApp/
 cd /tmp/TestChatApp
