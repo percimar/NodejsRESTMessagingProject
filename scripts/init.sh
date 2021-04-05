5ff9f3f6a5321398b7c0e511512b1c0241794704
Asmar <60093530@cna-qatar.edu.qa>
2021-04-05 14:49:18 +0300

    added lint config to cp

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,5 @@
 echo Starting init script
 set -e
 mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
-cp server.js testing.test.js package.json package-lock.json webfiles/ /tmp/TestChatApp -r
+cp server.js testing.test.js package.json package-lock.json .eslintrc.js webfiles/ /tmp/TestChatApp -r
 echo End of init script
