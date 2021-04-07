20426e3d1fb35010f7180e7964e5a1f5523d161c
Asmar <60093530@cna-qatar.edu.qa>
2021-04-07 16:14:47 +0300

    fixed --no-sandbox issue

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 echo Starting init script
 set -e
-echo "PWD: " pwd
-echo "USER: " whoami
-echo "Node: " which Node
+echo "PWD: " ${pwd}
+echo "USER: " ${whoami}
+echo "Node: " ${which node}
 mkdir /tmp/TestChatApp - p
 cp server.js testing.test.js puppeteer.js package.json package-lock.json .eslintrc.js webfiles/ /tmp/TestChatApp -r
 echo End of init script
