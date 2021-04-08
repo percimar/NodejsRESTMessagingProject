f6776369673bb580f483c75fb7b9d9d8424a8293
Asmar <60093530@cna-qatar.edu.qa>
2021-04-08 21:36:13 +0300

    sped up script

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,8 +6,8 @@ echo "USER: "
 whoami
 echo "Node: "
 which node
-npm install
+#npm install
 mkdir /tmp/TestChatApp -p
-ln -sf /var/lib/jenkins/workspace/CP4480\ Chat\ App\ Pipeline/node_modules/ /tmp/TestChatApp/node_modules  
+# ln -sf /var/lib/jenkins/workspace/CP4480\ Chat\ App\ Pipeline/node_modules/ /tmp/TestChatApp/node_modules  
 cp server.js axios.test.js puppeteer.test.js package.json package-lock.json .eslintrc.js webfiles/ /tmp/TestChatApp -r
 echo End of init script
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
-mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
+# mysql -u chatapp -p12class34 testjenkins < ./scripts/initdb.sql
 cd /tmp/TestChatApp
+npm install
 npx eslint server.js axios.test.js puppeteer.test.js
 npx jest axios
 npx jest puppeteer
