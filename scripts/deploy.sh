cb0bba569237317d1951d7a541fd9fd20eecb208
Asmar <60093530@cna-qatar.edu.qa>
2021-04-08 21:34:03 +0300

    STILL FAILING ASDKNASKDLASNIIEWNWE

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ mysql -u chatapp -p12class34 jenkins < ./scripts/db.sql
 cp webfiles/* /var/www/chatapp/ -r
 cp package.json package-lock.json /opt/ChatApp
 cd /opt/ChatApp
-npm install
+npm install --only=prod
 cp /var/lib/jenkins/workspace/CP4480\ Chat\ App\ Pipeline/server.js ./server.js
 echo End of deploy script
