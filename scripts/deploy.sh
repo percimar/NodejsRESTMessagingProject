2d985975377554b5738a7f6efcc4b4ab4652d4d9
Asmar <60093530@cna-qatar.edu.qa>
2021-04-08 20:04:56 +0300

    removed cleanup, changed deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,7 @@ set -e
 mysql -u chatapp -p12class34 jenkins < ./scripts/db.sql
 cp server.js package.json package-lock.json /opt/ChatApp
 cp webfiles/* /var/www/chatapp/ -r
+cd /opt/ChatApp
+npm install
+systemctl restart chatapp
 echo End of deploy script
